83d33018e3a2a2fb5ee2f8c5be7d33cbcb4b9181
ihollander <ianholla@gmail.com>
2021-04-30 16:08:59 -0400

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
-  get '/students/grades', to: 'students#grades'
-  get '/students/highest-grade', to: 'students#highest_grade'
   get '/students/:id', to: 'students#show'
 end
