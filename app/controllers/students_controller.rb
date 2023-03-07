a9427cbffbc91273e467f2d7dcb0098ee88df0b1
Shelby Talbert <shelby.talbert@flatironschool.com>
2023-03-07 11:57:24 -0500

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
 
   def index
     students = if params[:name]
-                 Student.by_name(params[:name])
+                 Student.find_by_last_name(params[:name].capitalize)
                else
                  Student.all
                end    
